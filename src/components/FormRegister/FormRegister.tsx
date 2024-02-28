16e93771ee036ec7498e76ef10634c6fed717b36
nayara-al <anna_nayara@hotmail.com>
2024-02-27 21:33:17 -0300

    form register - TO DO

--- src/components/FormRegister/FormRegister.tsx
+++ src/components/FormRegister/FormRegister.tsx
@@ -1,12 +1,46 @@
 import { PawPrint } from "@phosphor-icons/react";
 import style from "./FormRegister.module.css";
 import * as Component from "../index";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function FormRegister() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [repeatePassword, setRepeatePassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const handleRegister = (e: FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    /* const user = {
+      displayName,
+      email,
+      password,
+    }; */
+
+    if (password !== repeatePassword) {
+      setError("As senhas precisam ser iguais.");
+      return;
+    }
+
+    //const res = await createUser(user);
+
+    console.log(name, email, password, repeatePassword);
+    //console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
     <div className={style.container}>
       <h1 className={style.headTitle}>Cadastre-se para postar</h1>
-      <form className={style.formBase}>
+      <form className={style.formBase} onSubmit={handleRegister}>
         <div className={style.PawRegister}>
           <PawPrint size={40} color="white" weight="fill" />
         </div>
@@ -16,12 +50,16 @@ export default function FormRegister() {
             id="name"
             label="Nome"
             placeholder="Qual seu nome?"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
             required
           />
           <Component.FormFieldText
             id="email"
             label="E-mail"
             placeholder="Informe seu e-mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
             required
           />
           <Component.FormFieldText
@@ -30,6 +68,8 @@ export default function FormRegister() {
             placeholder="Escolha uma senha"
             required
             type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
           />
           <Component.FormFieldText
             id="repeatPassword"
@@ -37,6 +77,8 @@ export default function FormRegister() {
             placeholder="Confirme sua senha"
             required
             type="password"
+            value={repeatePassword}
+            onChange={(event) => setRepeatePassword(event.target.value)}
           />
           <Component.Button buttonType="secondary">Cadastrar</Component.Button>
         </div>
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,9 +1,10 @@
-import style from "./Post.module.css"
+import style from "./Post.module.css";
 export default function PostUnit() {
   return (
-    <div>
-        <img src='' alt=''/>
-        <p className={style.paragraph}>User</p>
+    <div className={style.containerPost}>
+      <div className={style.imagePost}>a</div>
+      {/* <img src='' alt=''/> */}
+      <p className={style.paragraph}>User</p>
     </div>
-  )
+  );
 }
